7d7d46814d8e906dc5efa69938c380209857d55c
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2023-01-21 19:10:51 +0100

    ajout vec serveur ok

--- src/serveur.rs
+++ src/serveur.rs
@@ -6,32 +6,33 @@ use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 // use std::str::from_utf8; // pour voir ce que le serveur reçoit
 
-fn handle_client(mut socket: TcpStream) {
+fn handle_client(mut socket: Vec::<TcpStream>) {
     const BUFFER: usize = 512;  // mem tampon à 512 octets
     let mut data = [0 as u8; BUFFER];
-    while match socket.read(&mut data) {
-        Ok(size) => {
-            // echo everything!
-            // remplacer le unwrap()
-            socket.write(&data[0..size]).unwrap();
-            // voir ce que reçoit le serveur
-            // let text = from_utf8(&data).unwrap();
-            // println!("Client : {}", text);
-            true
-        },
-        Err(_) => {
-            println!("Une erreur est survenue, déconnexion du client : {}", 
-                socket.peer_addr().unwrap());
-            // remplacer le unwrap()
-            socket.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
+    for n in 0..socket.len() {
+        while match socket[n].read(&mut data) {
+            Ok(size) => {
+                // echo everything!
+                // remplacer le unwrap()
+                socket[n].write(&data[0..size]).unwrap();
+                // voir ce que reçoit le serveur
+                // let text = from_utf8(&data).unwrap();
+                // println!("Client : {}", text);
+                true
+            },
+            Err(_) => {
+                println!("Une erreur est survenue, déconnexion du client : {}", 
+                    socket[n].peer_addr().unwrap());
+                // remplacer le unwrap()
+                socket[n].shutdown(Shutdown::Both).unwrap();
+                false
+            }
+        } {}
+    }
 }
 
 fn main() {
     // remplacer le unwrap()
-    let mut vec_socket = Vec::<TcpStream>::new();
     let listener = TcpListener::bind("0.0.0.0:25566").unwrap();
     //let listener_clone = listener.try_clone().unwrap();
     // accept connections and process them, spawning a new thread for each one
@@ -41,13 +42,14 @@ fn main() {
             Ok(socket) => {
                 // remplacer le unwrap()
                 let socket_clone = socket.try_clone().expect("Impossible de cloner");
+                let mut vec_socket = Vec::<TcpStream>::new();
                 vec_socket.push(socket_clone);
                 println!("Nouvelle connexion : {}", 
                     socket.peer_addr().unwrap());
                     //listener1.push(socket);
                 /*let thread_socket = */thread::spawn(move || {
                     // connection succeeded
-                    handle_client(socket)
+                    handle_client(vec_socket)
                 });
                 // let res = thread_socket.join();
             }
